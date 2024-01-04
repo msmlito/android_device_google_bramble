bf011a9fccecb8ff52e9be96dddb0e8484c500f3
neroices <git@slce.moe>
2024-01-04 23:32:50 +0700

    bramble: Initial RisingOS bringup

--- rising_bramble.mk
+++ rising_bramble.mk
@@ -4,9 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Lineage stuff.
+# Inherit some common RisingOS stuff.
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/rising/config/rising.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/bramble/aosp_bramble.mk)
@@ -17,7 +17,7 @@ include device/google/bramble/device-lineage.mk
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 4a (5G)
-PRODUCT_NAME := lineage_bramble
+PRODUCT_NAME := rising_bramble
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2340
